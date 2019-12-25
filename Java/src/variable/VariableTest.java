83e06edaa589344c1b01e188e93e089491c4eba9
shkim4738 <shkim4738@gmail.com>
2019-12-25 23:57:09 +0900

    Variable Basic

---
+++ Java/src/variable/VariableTest.java
@@ -0,0 +1,35 @@
+package variable;
+
+public class VariableTest {
+	public static void main(String[] args) {
+		int age;
+		
+		age = 30;
+		
+		System.out.println(age);
+		
+		age = 10;
+		
+		System.out.println(age);
+		
+		int age2 = 10;
+		
+		System.out.println(age2);
+		
+		int age3, count;
+		
+		age3 = 10;
+		count = 20;
+		
+		System.out.println(age3 + ", " + count);
+		
+		// int : 4bytes 메모리 사용
+		
+		int numberOfStudent;
+		
+		numberOfStudent = 40;
+		
+		System.out.println(numberOfStudent);
+		
+	}
+}
