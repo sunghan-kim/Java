91414207481412800330ec99831c04ef0a05d3f0
shkim4738 <shkim4738@gmail.com>
2019-12-26 21:48:50 +0900

    Float & Double & Boolean Data Type

---
+++ Java/src/variable/BooleanTest.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+		
+		boolean isMarried = false;
+		
+		System.out.println(isMarried);
+		
+	}
+	
+}
---
+++ Java/src/variable/DoubleTest2.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class DoubleTest2 {
+
+	public static void main(String[] args) {
+		
+		double dNum = 1;
+		
+		for (int i=0; i < 10000; i++) {
+			
+			dNum = dNum + 0.1;
+			
+		}
+		
+		System.out.println(dNum); // 1001.000000000159 <- 부동 소수점 오차
+		
+	}
+	
+}
