f76a68a78ad037049fbeeccc62bf529f9cfb606d
shkim4738 <shkim4738@gmail.com>
2019-12-26 22:38:58 +0900

    부호 연산자(Ex1), 산술 연산자(Ex2), 복합 대입 연산자(Ex3), 단항 연산자(Ex4)

---
+++ Java/src/operator/OperatorEx3.java
@@ -0,0 +1,23 @@
+package operator;
+
+public class OperatorEx3 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 복합 대입 연산자
+		 * 
+		 *   - +=
+		 *   - -=
+		 *   - *=
+		 */
+		
+		int num1 = 10;
+		
+		num1 += 2;
+		
+		System.out.println(num1);
+		
+	}
+	
+}
