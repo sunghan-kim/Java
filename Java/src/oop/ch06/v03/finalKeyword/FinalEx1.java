1b08e82a4c98386ba9b89f30f2c79d354e97e4dc
shkim4738 <shkim4738@gmail.com>
2020-01-05 19:08:27 +0900

    final 예약어

---
+++ Java/src/oop/ch06/v03/finalKeyword/FinalEx1.java
@@ -0,0 +1,38 @@
+package oop.ch06.v03.finalKeyword;
+
+public class FinalEx1 {
+
+	/*
+	 * 
+	 *   # final 예약어
+	 *   
+	 *     - 하위 클래스에서 final 예약어를 지정한 변수나 메서드를 재정의할 수 없음
+	 *     
+	 *     ## final 변수
+	 *     
+	 *       - 값이 변경될 수 없는 상수
+	 *       
+	 *         public static final double PI = 3.14;
+	 *         
+	 *           - 오직 한 번만 값을 할당할 수 있음
+	 *           
+	 *           
+	 *     ## final 메서드
+	 *     
+	 *       - 하위 클래스에서 재정의(overriding) 할 수 없음
+	 *       
+	 *       
+	 *    ## final 클래스
+	 *    
+	 *      - final 클래스는 더 이상 상속되지 않음
+	 *    
+	 *        ex) java의 String 클래스
+	 *        
+	 *        
+	 *    ## public static final 상수 값 정의하여 사용하기
+	 *    
+	 *      - 프로젝트 구현 시 여러 파일에서 공유해야 하는 상수 값은 하나의 파일에 선언하여 사용하면 편리함
+	 * 
+	 */
+	
+}
