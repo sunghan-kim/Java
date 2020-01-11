abbcee38d97988c04fa5382600b2d813f96ced96
shkim4738 <shkim4738@gmail.com>
2020-01-12 02:06:13 +0900

    TreeSet - Comparable, Comparator 이용 정렬 구현

---
+++ Java/src/oop/ch08/javaBasicClass/v11/treeSet/MemberTreeSet2.java
@@ -0,0 +1,41 @@
+package oop.ch08.javaBasicClass.v11.treeSet;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class MemberTreeSet2 {
+
+	private TreeSet<Member2> treeSet;
+	
+	public MemberTreeSet2() {
+		treeSet = new TreeSet<Member2>(new Member2());
+	}
+	
+	public void addMember(Member2 member) {
+		treeSet.add(member);
+	}
+	
+	public boolean removeMember(int memberId) {
+		Iterator<Member2> ir = treeSet.iterator();
+		while(ir.hasNext()) {
+			Member2 member = ir.next();
+			
+			if (member.getMemberId() == memberId) {
+				treeSet.remove(member);
+				return true;
+			}
+		}
+		
+		System.out.println(memberId + "번호가 존재하지 않습니다.");
+		return false;
+	}
+	
+	public void showAllMember() {
+		
+		for (Member2 member : treeSet) {
+			System.out.println(member.toString());
+		}
+		
+	}
+	
+}
