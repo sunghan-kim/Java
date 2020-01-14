615525ddeb14a1c6dbdfa8125ef5cc4a7d63fcd9
shkim4738 <shkim4738@gmail.com>
2020-01-15 00:04:56 +0900

    예외 클래스 (Exception Class)

---
+++ Java/src/oop/ch11/exception/v02/exceptionClass/ExceptionClassEx1.java
@@ -0,0 +1,27 @@
+package oop.ch11.exception.v02.exceptionClass;
+
+public class ExceptionClassEx1 {
+
+	/*
+	 * 
+	 * # 예외 클래스
+	 * 
+	 *   - 모든 예외 클래스의 최상위 클래스는 "Exception" 클래스
+	 *   
+	 *   
+	 *   											  Exception
+	 *   												|
+	 *   					  --------------------------------------------------------
+	 *   					  |														 |
+	 *   					  |														 |
+	 *   				입출력 예외 처리									실행 오류 예외 처리
+	 *   				  (IOException)										 (RuntimeException)
+	 *   					  |														 |
+	 *   	      -------------------------- 						 --------------------------------
+	 *   		  |						   |						 |								|
+	 *   		  |						   |						 |								|
+	 *   FileNotFoundException		SocketExceptiton		ArithmeticExceptioin		IndexOutofBoundsException
+	 * 
+	 */
+	
+}
