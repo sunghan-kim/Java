250ac3832cc0645c183c849efbe1cf7de664b13c
shkim4738 <shkim4738@gmail.com>
2020-01-15 00:04:31 +0900

    예외 처리 - 에러(Erro)와 예외(Exception)

---
+++ Java/src/oop/ch11/exception/v01/errorException/ErrorExceptionEx1.java
@@ -0,0 +1,32 @@
+package oop.ch11.exception.v01.errorException;
+
+public class ErrorExceptionEx1 {
+
+	/*
+	 * 
+	 * # 오류와 예외 클래스
+	 * 
+	 *   - 시스템 오류 (error)
+	 *   
+	 *     - 가상 머신에서 발생
+	 *     - 프로그래머가 처리할 수 없음
+	 *       ex1) 동적 메모리를 다 사용한 경우
+	 *       ex2) stack over flow 등
+	 *     
+	 *   - 예외 (Exception)
+	 *   
+	 *     - 프로그램에서 제어할 수 있는 오류
+	 *       ex1) 읽으려는 파일이 없는 경우
+	 *       ex2) 네트워크나 소켓 연결 오류
+	 *       
+	 *     - 자바 프로그램에서는 예외에 대한 처리를 수행함
+	 *     
+	 *       Throwable
+	 *     		 |
+	 *     --------------
+	 *     |			|
+	 *   Error		Exception
+	 * 
+	 */
+	
+}
